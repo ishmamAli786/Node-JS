834f1a2798446a36cf2cab02e0bac9d829f6d9b9
ishmam ali <ishmamalikhan@gmail.com>
2020-09-04 15:41:11 +0500

    REPL

---
+++ rapel.js
@@ -0,0 +1,11 @@
+/// REpl
+/// 1. JS Expression
+/// use variables
+//// multiline code
+//// use (-) to get the last result
+//// we can use editor mode
+
+
+
+// repl is a envoirment where we can debug and experiment javscript
+
