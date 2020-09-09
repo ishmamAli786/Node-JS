12df5fe0e605fbfa3859bd85dede966a3355c7ac
ishmam ali <ishmamalikhan@gmail.com>
2020-09-09 09:50:32 +0500

    call Back

---
+++ callBack.js
@@ -0,0 +1,14 @@
+// var  sum=function (a,b)
+//     return a+b;
+// 
+
+function complexExample(sum){
+    console.log(sum(200,300));
+}
+
+
+complexExample(function (a, b){
+     return a+b;
+    });
+
+// console.log(sum(5, 6));
