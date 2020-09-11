3764dccdf01b1e893cabada5d1fd1cf57c9a229f
ishmam ali <ishmamalikhan@gmail.com>
2020-09-11 15:45:39 +0500

    path Module

---
+++ pathModule.js
@@ -0,0 +1,9 @@
+const path=require('path');
+console.log(path.dirname('D:\Node Js\pathModule.js'));
+console.log(path.extname('D:\Node Js\pathModule.js'));
+console.log(path.basename('D:\Node Js\pathModule.js'));
+
+
+
+
+console.log(path.parse('D:\Node Js\pathModule.js'))
