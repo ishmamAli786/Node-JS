f860110aee501c6bdc53fd86f392c655a798b04b
ishmam ali <ishmamalikhan@gmail.com>
2020-09-12 20:29:27 +0500

    Create Own Module

---
+++ importexport/index.js
@@ -0,0 +1,5 @@
+const {add,sub,mult}=require('./opr');
+console.log(add(5,5));
+console.log(sub(10, 5));
+console.log(mult(10, 5));
+// console.log(oper);
---
+++ importexport/opr.js
@@ -0,0 +1,15 @@
+const add=(a,b)=>{
+    return a+b;
+}
+// const name="ishmam";
+const sub = (a, b) => {
+    return a - b;
+}
+const mult = (a, b) => {
+    return a * b;
+}
+// module.exports.add = add; 
+// module.exports.sub = sub; 
+// module.exports.mult = mult;
+
+module.exports= {add,sub,mult}
