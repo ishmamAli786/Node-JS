3ca5e615aac347b10ba58231d6086d2d9cfcbfdf
Ishmam Ali <56753433+ishmamAli786@users.noreply.github.com>
2020-09-21 08:19:02 -0700

    Event Module

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const fs=require('fs');
-const http=require('http');
-const server=http.createServer((req,res)=>{
-//    console.log(req.url);
- if (req.url="/userApi"){
-    fs.readFile(`userApi.json`,"utf-8",(err,data)=>{
-        console.log(err);
-        console.log(data);
-        res.end();
-    })
-}else{
-    res.writeHead(404,{"content-type":"text/html"});
-    res.end("<h1> 404 error pages. page does not exist</h1>");
-}
-});
-server.listen(8003,'127.0.0.1',()=>{
-    console.log('listing the port number 8000')
-});
+/// event module
+/// node.js has a build in module, called events
+/// where you can create fire and listen for your own events
+/// example-1 registering for the event to be fired only one time using oncd.
+/// example-2 create an event emitter instance and register a couple of callbacks
+/// example-3 registering for the event with callback parameters
+
+
+const EventEmitter=require('events');
+const event=new EventEmitter();
+event.on('sayMyName',()=>{
+    console.log('my name is ishmama ali khan')
+})
+event.on('sayMyName',()=>{
+    console.log('my name is ishmama ali khan')
+})
+event.on('sayMyName',()=>{
+    console.log('my name is ishmama ali khan')
+})
+event.on('checkPage',(sc,msg)=>{
+    console.log(`status code is ${sc} and the page is ok`)
+})
+event.emit('checkPage',200,'ok');
